9e59f90b29b8c44eec490f7740cff28befd10e59
drvcs <drey@vc-soft.com>
2019-08-08 15:01:24 -0500

    showing shipping prices

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,47 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens.'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras.'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '.'
+  },
+  {
+    name: 'Motorola G10',
+    price: 500,
+    description: 'Great functionalitis and accessibility operations.'
+  },
+  {
+    name: 'Xiaomi',
+    price: 350,
+    description: 'Just a Phone.'
+  },
+  {
+    name: 'Zoom',
+    price: 299,
+    description: 'This is a calculator.'
+  },
+  {
+    name: 'Iphone X',
+    price: 1000,
+    description: 'Last Iphone version.'
+  },
+  {
+    name: 'Haweii P30',
+    price: 890,
+    description: 'The best camera on the market.'
+  },
+  {
+    name: 'Samsung Galaxy S10',
+    price: 990,
+    description: 'Best performance and battery duration.'
   }
 ];
 
